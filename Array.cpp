640e67195c61d6a7648e7b782be8d97f8876b49e
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-06 15:06:43 +0700

    changes array index 1

--- Array.cpp
+++ Array.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main(){
-
 int aku[5] = {0,20,30,40,50};
-
 cout << "Data Pada Index 1 = " << aku[1] << endl;
+
+aku[1] = 200;
+cout << "Data pada index 1 = " << aku[1] << endl;
+
 }

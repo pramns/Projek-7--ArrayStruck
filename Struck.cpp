f7f5f96f63155a3c5e8e91c562d4c0c3ef7a89e0
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-06 15:36:52 +0700

    output mhs

--- Struck.cpp
+++ Struck.cpp
@@ -16,6 +16,10 @@ int main(){
     cout << "Umur : ";
     cin >> mhs.umur;
     cout << endl;  
+
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Alamat : " << mhs.alamat << endl;
+    cout << " Umur : " << mhs.umur << endl;
 }
 
 
